176cd97d8068ebbd1a541381e6b8aade2fc3c127
VNalu <vnpolloc@calpoly.edu>
2021-04-13 21:38:59 -0700

    fixed incorrect order of containers starting bug

--- Proxy-app/__init__.py
+++ Proxy-app/__init__.py
@@ -5,7 +5,6 @@ import time
 app = Flask(__name__)
 
 # Create connection to websec app
-time.sleep(3) # TODO: Fix docker-compose to run websec before proxy always
 address = ("web", 5001)
 connection = Client(address, authkey=b"aliens-are-already-here")
 connection.send("test") # DEBUG: Test message (remove later)
