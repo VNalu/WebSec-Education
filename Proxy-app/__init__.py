af773d3199083685fc2d2fad7014c287cc118a4c
VNalu <vnpolloc@calpoly.edu>
2021-04-15 18:23:21 -0700

    remove connection (switching to API)

--- Proxy-app/__init__.py
+++ Proxy-app/__init__.py
@@ -1,10 +1,3 @@
 from flask import Flask
-from multiprocessing.connection import Client
-import time
 
 app = Flask(__name__)
-
-# Create connection to websec app
-address = ("web", 5001)
-connection = Client(address, authkey=b"aliens-are-already-here")
-connection.send("test") # DEBUG: Test message (remove later)
--- WebSec-app/__init__.py
+++ WebSec-app/__init__.py
@@ -1,9 +1,3 @@
 from flask import Flask
-from multiprocessing.connection import Listener
 
 app = Flask(__name__)
-
-# Create connection to proxy
-address = ("0.0.0.0", 5001)
-listener = Listener(address, authkey=b"aliens-are-already-here")
-connection = listener.accept()
