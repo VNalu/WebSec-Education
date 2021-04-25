71c960653f3d04499b0f6fbac59ece61135f9c1a
VNalu <vnpolloc@calpoly.edu>
2021-04-24 17:12:42 -0700

    handle new request

--- WebSec-app/views.py
+++ WebSec-app/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from . import app
 
 @app.route("/")
@@ -23,3 +23,15 @@ def xss_lesson1():
 @app.route("/lessons/cross-site-scripting-lab1")
 def xss_lab1():
     return render_template("labs/xss1.html")
+
+@app.route('/api/v1/request/new', methods=['POST'])
+def new_request():
+    data = request.get_json() # TODO: Parse json
+    if not validate_session(data["session_id"]):
+        return {"status": "error", "error": "invalid session id"}
+    
+    # TODO: Add request to user queue
+    return {"status": "success"}
+
+# TODO: Make UI for request
+# TODO: Figure out forward editted request for proxy 
