4e75fb6516750686dc00450b54b21bc1903bfa1d
VNalu <vnpolloc@calpoly.edu>
2021-05-12 19:38:23 -0700

    set up web socket connection between browser and webapp

--- WebSec-app/views.py
+++ WebSec-app/views.py
@@ -1,18 +1,19 @@
 from datetime import datetime
-from flask import Flask, render_template, url_for, request,flash, jsonify
-from . import app
+from flask import Flask, render_template, url_for, request,flash, jsonify, Blueprint
 from queue import Queue
 import sys
 
 requestsToEditQueue = Queue()
 
-@app.route("/")
+views = Blueprint("views", __name__, template_folder="templates")
+
+@views.route("/")
 def home():
     return render_template("index.html")
 
 # Leaving hello in as an example
-@app.route("/hello/")
-@app.route("/hello/<name>")
+@views.route("/hello/")
+@views.route("/hello/<name>")
 def hello_there(name = None):
     return render_template(
         "hello_there.html",
@@ -20,17 +21,17 @@ def hello_there(name = None):
         date=datetime.now()
     )
 
-@app.route("/lessons/cross-site-scripting-lesson1")
+@views.route("/lessons/cross-site-scripting-lesson1")
 def xss_lesson1():
     return render_template("lessons/xss1.html")
 
-@app.route("/lessons/cross-site-scripting-lab1")
+@views.route("/lessons/cross-site-scripting-lab1")
 def xss_lab1():
     packet_to_edit = "This is where the packet can be edited."
     cookie_to_edit = "This is where the cookies can be edited."
     return render_template("labs/xss1.html", packet=packet_to_edit, cookies=cookie_to_edit)
 
-@app.route('/api/v1/request/new', methods=['POST'])
+@views.route('/api/v1/request/new', methods=['POST'])
 def new_request():
     print("Raw: ", request.json, file=sys.stderr)
     data = request.json
@@ -44,7 +45,7 @@ def new_request():
     return {"status": "success"}
 
 # TODO: Write function to put data in data structure callable by javascript
-@app.route('/api/v1/request', methods=['GET'])
+@views.route('/api/v1/request', methods=['GET'])
 def get_request():
     if requestsToEditQueue.empty():
         return jsonify({"request": None})
@@ -52,7 +53,7 @@ def get_request():
 
 # TODO: Validate session
 def validate_session(sess_id):
-    if sess_id == "123":
+    if sess_id == 123:
         print("Good session id", file=sys.stderr)
         return True
     print("Bad session id", file=sys.stderr)
--- WebSec-app/webapp.py
+++ WebSec-app/webapp.py
@@ -1,6 +1,16 @@
 # Entry point for the application.
-from . import app    # For application discovery by the 'flask' command. 
-from . import views  # For import side-effects of setting up routes. 
+# from . import app    # For application discovery by the 'flask' command. 
+from views import views  # For import side-effects of setting up routes. 
+from flask import Flask
+from flask_socketio import SocketIO
 
 # Time-saver: output a URL to the VS Code terminal so you can easily Ctrl+click to open a browser
 # print('http://127.0.0.1:5000/hello/VSCode')
+
+app = Flask(__name__)
+app.register_blueprint(views)
+app.secret_key = "aintreallyasecret" 
+socketio = SocketIO(app)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, host="0.0.0.0")
